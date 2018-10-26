7790d8247558d7afc74c9eb42db1335e34a1196a
Yura Panarin <tduble94@gmail.com>
2018-10-26 23:57:44 +0200

    test(component): add test

--- lib/src/components/virtual-table.component.spec.ts
+++ lib/src/components/virtual-table.component.spec.ts
@@ -1,5 +1,5 @@
 import { VirtualTableComponent } from './virtual-table.component';
-import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { ComponentFixture, async, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { NgVirtualTableService } from '../services/ngVirtualTable.service';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,6 +8,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DynamicModule } from 'ng-dynamic-component';
+import { VirtualTableConfig } from '../interfaces';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { SimpleChange } from '@angular/core';
 describe('VirtualTableComponent', () => {
   let fixture: ComponentFixture<VirtualTableComponent>;
   let component: VirtualTableComponent;
@@ -44,5 +48,105 @@ describe('VirtualTableComponent', () => {
 
   it('should exist', () => {
     expect(fixture).toBeTruthy();
+  });
+
+  describe('applyConfig', () => {
+    it(
+      'should apply config',
+      fakeAsync(() => {
+        const config: VirtualTableConfig = {
+          header: false,
+        };
+
+        const config2: VirtualTableConfig = {
+          header: true,
+        };
+
+        const dataSource = of(
+          Array(100).fill(0).map((e) => ({
+            name: Math.random().toString(36).substring(7),
+            age: Math.round(Math.random() * 1000),
+          })),
+        );
+
+        component.config = config;
+        component.dataSource = dataSource;
+
+        component.ngOnChanges({
+          config: new SimpleChange(null, component.config, false),
+          dataSource: new SimpleChange(null, component.dataSource, false),
+        });
+        fixture.detectChanges();
+        tick(1);
+        const header = fixture.nativeElement.querySelector('.header');
+
+        expect(header).toBe(null);
+
+        component.applyConfig(config2);
+        fixture.detectChanges();
+
+        const headerAfter = fixture.nativeElement.querySelector('.header');
+
+        expect(headerAfter).not.toBe(null);
+      }),
+    );
+  });
+
+  describe('Template', () => {
+    it(
+      'should not show header',
+      fakeAsync(() => {
+        const config: VirtualTableConfig = {
+          header: false,
+        };
+
+        const dataSource = of(
+          Array(100).fill(0).map((e) => ({
+            name: Math.random().toString(36).substring(7),
+            age: Math.round(Math.random() * 1000),
+          })),
+        );
+
+        component.config = config;
+        component.dataSource = dataSource;
+
+        component.ngOnChanges({
+          config: new SimpleChange(null, component.config, false),
+          dataSource: new SimpleChange(null, component.dataSource, false),
+        });
+        fixture.detectChanges();
+        tick(1);
+        const header = fixture.nativeElement.querySelector('.header');
+
+        expect(header).toBe(null);
+      }),
+    );
+
+    it(
+      'should show header',
+      fakeAsync(() => {
+        const config: VirtualTableConfig = {};
+
+        const dataSource = of(
+          Array(100).fill(0).map((e) => ({
+            name: Math.random().toString(36).substring(7),
+            age: Math.round(Math.random() * 1000),
+          })),
+        );
+
+        component.config = config;
+        component.dataSource = dataSource;
+
+        component.ngOnChanges({
+          config: new SimpleChange(null, component.config, false),
+          dataSource: new SimpleChange(null, component.dataSource, false),
+        });
+        fixture.detectChanges();
+        tick(1);
+        const header = fixture.nativeElement.querySelector('.header');
+
+        expect(header).not.toBe(null);
+      }),
+    );
   });
 });
